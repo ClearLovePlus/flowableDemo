f4d8be2443ce5c16703234483e805d3a28420abb
chenhao <chenhao3@tebon.com.cn>
2021-10-22 17:12:58 +0800

    flowable 初始化

---
+++ flowable/src/test/java/com/flb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flb.demo;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
